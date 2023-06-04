0dad9ea8c238bc461346034f6309967c83817e45
tetsu <root2120@gmail.com>
2023-06-04 14:52:18 +0900

    Argsの利用

--- react-storybook/src/components/Button/Button.stories.jsx
+++ react-storybook/src/components/Button/Button.stories.jsx
@@ -13,19 +13,52 @@ export const HelloButton = () =>
 export const ClickButton = () => 
     <Button>Click!</Button>;
 
-export const Default = () => 
-    <Button>Default</Button>;
 
+
+//Argsの利用
+//Template関数を定義する。
+const Template = ( args ) =>    //argsはコンポーネントへ渡すprops
+    <Button { ...args } />;
+
+//ストーリーのDefault関数をTemplate関数でbindすることで新規の関数として作成する。
+//作成後、argsを利用してpropsの値を設定する。
+export const Default = Template.bind( {} );
+Default.args = {
+    children:'Default',
+};
+
+export const Danger = Template.bind( {} );
+Danger.args = {
+    children: 'Danger',
+    color: 'danger',
+};
+
+
+//従来の記述に比べて、共通個所を割愛した記述ができるため簡略化することができる。
+//渡すpropsが多くなったり、記述が複雑化している場合に有効
+export const Primary = Template.bind( {} );
+Primary.args = {
+    children:'Primary',
+    color:'primary',
+};
+
+/*前の記述
 export const Primary = () =>
     <Button color="primary">Primary</Button>;
+*/
 
-export const Danger = () =>
-    <Button color="danger">Danger!!</Button>;
+export const PrimarySmall = Template.bind( {} );
+PrimarySmall.args = {
+    children: 'Small',
+    color: 'primary',
+    size:'sm',
+};
 
-export const PrimarySmall = () => (
-    <Button size="sm" color="primary">primary small</Button>
-);
 
-export const PrimaryLarge = () => (
-    <Button size="lg" color="primary">primary large</Button>
-);
+export const PrimaryLarge = Template.bind( {} );
+PrimaryLarge.args = {
+    children: 'Large',
+    color:'primary',
+    size:'lg',
+};
+
